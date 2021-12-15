c0406892de1cd0dac36bc20dad951cefb27d5cfc
Ritika Nagpal <ritika.nagpal4@gmail.com>
2021-12-15 19:14:14 +0530

    project details page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from "../screens/home/Home";
-// import Details from "../screens/details/Details";
+import Details from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
-// import Confirmation from "../screens/confirmation/Confirmation";
+import Confirmation from "../screens/confirmation/Confirmation";
 
 const Controller = () => {
   const baseUrl = "/api/v1/";
@@ -16,18 +16,18 @@ const Controller = () => {
           path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         />
-        {/* <Route
+        <Route
           path="/movie/:id"
           render={(props) => <Details {...props} baseUrl={baseUrl} />}
-        /> */}
+        />
         <Route
           path="/bookshow/:id"
           render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
         />
-        {/* <Route
+        <Route
           path="/confirm/:id"
           render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-        /> */}
+        />
       </div>
     </Router>
   );
